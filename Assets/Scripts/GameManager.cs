89df76961d9f7ea074940d295e6f877638091333
Thomas Steinert <hello@chroni.cc>
2022-06-06 13:40:00 +0200

    chore(ui): use a custom level for ui
    When the ui is contained in it's own level, it can be edited in one
    place and loaded during the loading of every playable level.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     private void Awake()
     {
         DataHandler.LoadGameData();
+
+        if (!SceneManager.GetSceneByName("UI").isLoaded)
+        {
+            SceneManager.LoadSceneAsync("UI", LoadSceneMode.Additive);
+        }
     }
 }
